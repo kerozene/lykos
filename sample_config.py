b5b2d97d9964bf4f21f602a9841362a94113851c
Kyle Fahringer <me@sharky.so>
2013-11-25 09:54:53 -0500

    add a few things to sample config
    * admins now can be specified by host or acct (nickserv)
    * ops_opchan is a thing (mebbe)
    * nyu is ugly

--- sample_config.py
+++ sample_config.py
@@ -10,12 +10,12 @@
 # ===================================================
 
 # The network for lykos to run on.
-network    = "irc.freenode.net"
+network    = "irc.freenode.net"  # if you don't know what this is, why are you running an irc bot
 port       = 6667
-serverpass = "" #leave blank if none
+serverpass = ""  # leave blank if none
 
 # The channel for lykos to join.
-channels   ="#channel"
+channels   = "#channel"
 opchan     = "#channel-ops"
 
 # Bot information.
@@ -24,8 +24,11 @@ nickpass   = "password"
 username   = "lykos"
 cmdchar    = "!"
 
-#Admin Information
-owner      = ("unaffiliated/owner",) #leave the trailing comma if there is 1 owner
-admin      = ("unaffiliated/admin1", "unaffiliated/admin2")
+# Admin Information
+owner_host = ("unaffiliated/owner",)  # leave the trailing comma if there is 1 owner
+owner_acct = ("owner",)  # this is the owners nickserv account, can be used as an alternate to their cloak.
+admin_host = ("unaffiliated/admin1", "unaffiliated/admin2")
+admin_acct = ("admin1", "admin2")  # same as owner_a, uses nickserv accounts and only works if idented
 
-
+# Alternate toggles
+ops_opchan = True  # if true, everyone that is *VOICED* in opchan will be considered a bot op.
